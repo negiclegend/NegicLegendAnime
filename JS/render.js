8d5af0189397373bf4428b03807a85cae487d2af
Negic Legend <negiclegend2@gmail.com>
2024-10-19 19:30:48 +0700

    fix import link

--- JS/render.js
+++ JS/render.js
@@ -1,6 +1,6 @@
 "use strict"
 import { $ } from './bind.js'
-import animeList from '../Storage/List/AnimeList.js'
+import animeList from './Storage/List/AnimeList.js'
 
 (() => {
     const display = $('#quote')
