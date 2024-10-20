58e1514f0ff1f6da70a96762cce7bf0d3d5991e4
Negic Legend <negiclegend2@gmail.com>
2024-10-20 21:58:31 +0700

    fix scroll button bug and upgrade scrolll.js

--- JS/scroll.js
+++ JS/scroll.js
@@ -4,17 +4,16 @@ import { $ } from './bind.js'
 (() => {
     const header = $('#header')
     const menu = $('#menu')
+    const scroll = $('#scroll')
     let lastScrollTop = 0
-    $('#scroll').addEventListener('click', () => window.scrollY > screen.height * 2.5 ? window.scrollTo({ top: 0, behavior: "smooth" }) : window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" }))
+    scroll.addEventListener('click', () => window.scrollY > screen.height * 2.5 ? window.scrollTo({ top: 0, behavior: "smooth" }) : window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" }))
     window.addEventListener('scroll', () => {
-        $('#scroll i').style.transform = window.scrollY > screen.height * 2.5 ? 'rotate(180deg)' : 'rotate(0deg)'
+        scroll.classList.toggle('up', window.scrollY > screen.height * 2.5)
         header.classList.toggle('fly', window.scrollY > header.offsetHeight)
         const currentScrollTop = document.documentElement.scrollTop
-        const isScrollingDown = currentScrollTop > lastScrollTop && window.scrollY > header.offsetHeight
-        let setTime
-        if(isScrollingDown) {
+        if(currentScrollTop > lastScrollTop && window.scrollY > header.offsetHeight) {
             menu.style.opacity = 0
-            setTime = setTimeout(() => (window.scrollY > header.offsetHeight) && (menu.style.display = 'none'), 300)   
+            setTimeout(() => (window.scrollY > header.offsetHeight) && (menu.style.display = 'none'), 300)   
         } else {
             menu.style.display = 'flex'
             setTimeout(() => menu.style.opacity = 1)
