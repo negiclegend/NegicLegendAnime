9fef55b368daf14f9f75c103edee830c80eade1f
Negic Legend <negiclegend2@gmail.com>
2024-10-20 11:49:08 +0700

    fix audio bug

--- JS/audio.js
+++ JS/audio.js
@@ -3,17 +3,30 @@ import { $, $$ } from './bind.js'
 
 export default(() => {
     let playing = {canPlay: false}
+    let playable = false
     let currentIndex
     const anms = $$('.anime-wrapper')
     const audio = $('#audio')
 
+    audio.addEventListener('canplaythrough', () => {
+        if (audio.readyState >= 3) {
+            playable = true
+        }
+    })
+
+    async function waitUntilPlayable() {
+        while (!playable) await new Promise(resolve => setTimeout(resolve, 100))
+    }
+
     anms.forEach((anm, i) => {
-        anm.addEventListener('mouseenter', () => {
+        anm.addEventListener('mouseenter', async () => {
             if (!playing.canPlay) return
             if (currentIndex !== i) {
+                playable = false
                 currentIndex = i
                 audio.src = `./Storage/Music/${i + 1}.mp3`
             }
+            await waitUntilPlayable()
             audio.play()
         })
         anm.addEventListener('mouseleave', () => !audio.paused && audio.pause())
